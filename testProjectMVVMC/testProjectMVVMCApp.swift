9ae89c2cd7ac7e25938d9f328a893f4977f99e45
matriplexim <matriplexim@users.noreply.github.com>
2022-12-03 18:31:48 +0400

    Initial Commit

---
+++ testProjectMVVMC/testProjectMVVMCApp.swift
@@ -0,0 +1,17 @@
+//
+//  testProjectMVVMCApp.swift
+//  testProjectMVVMC
+//
+//  Created by Максим Ломакин on 03.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct testProjectMVVMCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
